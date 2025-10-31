67f45ff3cc8c3d8131818ef37b9540055f6df351
Santy401 <santiprocastellar6@gmail.com>
2025-10-31 01:53:51 +0000

    Update _Cors.ts

--- app/api/_cors.ts
+++ app/api/_cors.ts
@@ -1,7 +1,6 @@
 export function setCorsHeaders(response: { headers: { set: (arg0: string, arg1: string) => void; }; }) {
-    response.headers.set("Access-Control-Allow-Origin", "https://tu-frontend.vercel.app");
-    response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    response.headers.set("Access-Control-Allow-Headers", "Content-Type");
-    return response;
-  }
-  
+  response.headers.set("Access-Control-Allow-Origin", "https://riosbackendtask.vercel.app");
+  response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  response.headers.set("Access-Control-Allow-Headers", "Content-Type");
+  return response;
+}
