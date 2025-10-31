8034ae83acf389c05a9bf917c4ef12f27c33f8c6
Santy401 <santiprocastellar6@gmail.com>
2025-10-31 01:59:58 +0000

    Add Login Cors

--- app/api/_cors.ts
+++ app/api/_cors.ts
@@ -1,5 +1,5 @@
 export function setCorsHeaders(response: { headers: { set: (arg0: string, arg1: string) => void; }; }) {
-  response.headers.set("Access-Control-Allow-Origin", "https://riosbackendtask.vercel.app");
+  response.headers.set("Access-Control-Allow-Origin", "https://riosbackendtask.vercel.app, https://riosbackendtask.vercel.app/ui/pages/Login/" );
   response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   response.headers.set("Access-Control-Allow-Headers", "Content-Type");
   return response;
